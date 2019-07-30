bf6cb432d34a763f7b620bcb47a4477b342e831c
JonasDreessen <dreessenjonas95@gmail.com>
2019-07-30 09:40:07 +0200

    add parameters and default value

--- api.php
+++ api.php
@@ -1,12 +1,23 @@
 <?php 
 header("Access-Control-Allow-Origin: *");
 $randomArray = [];
-for ($x = 0; $x <= 10; $x++) {
-    $dynamicalCreatedData = [];
-    $randomNumber = mt_rand(-10, 30);
-    array_push($dynamicalCreatedData,$x ,$randomNumber);
-    array_push($randomArray, $dynamicalCreatedData);
-} 
+
+
+if ($_GET["number"] === NULL){
+    for ($x = 0; $x <= 10; $x++) {
+            $dynamicalCreatedData = [];
+            $randomNumber = mt_rand(-10, 30);
+            array_push($dynamicalCreatedData,$x ,$randomNumber);
+            array_push($randomArray, $dynamicalCreatedData);
+        } 
+}else {
+    for ($x = 0; $x <= $_GET["number"]; $x++){
+        $dynamicalCreatedData = [];
+        $randomNumber = mt_rand(-10, 30);
+        array_push($dynamicalCreatedData,$x ,$randomNumber);
+        array_push($randomArray, $dynamicalCreatedData);
+ } 
+}
 
 $myJSON = json_encode($randomArray);
 echo $myJSON;
